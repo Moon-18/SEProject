464c5bddeba9d9677b38c31e0ccc4e7478a94d6c
huangyi <hyiii0204@gmail.com>
2023-03-08 21:50:48 +0800

    Yi Huang updates code reading

--- app/src/main/java/net/micode/notes/model/WorkingNote.java
+++ app/src/main/java/net/micode/notes/model/WorkingNote.java
@@ -102,6 +102,14 @@ public class WorkingNote {
     private static final int NOTE_MODIFIED_DATE_COLUMN = 5;
 
     // New note construct
+    /**
+     * @author:  Yi Huang
+     * @methodsName: WorkingNote
+     * @description: 构造函数
+     * @param: Context context, long noteId
+     * @return:
+     * @throws:
+     */
     private WorkingNote(Context context, long folderId) {
         mContext = context;
         mAlertDate = 0;
@@ -114,7 +122,16 @@ public class WorkingNote {
         mWidgetType = Notes.TYPE_WIDGET_INVALIDE;
     }
 
+
     // Existing note construct
+    /**
+     * @author:  Yi Huang
+     * @methodsName: WorkingNote
+     * @description: 构造函数
+     * @param: Context context, long noteId, long folderId
+     * @return:
+     * @throws:
+     */
     private WorkingNote(Context context, long noteId, long folderId) {
         mContext = context;
         mNoteId = noteId;
@@ -124,7 +141,17 @@ public class WorkingNote {
         loadNote();
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: loadNote
+     * @description: 加载已有的Note
+     * @param:
+     * @return: void
+     * @throws:
+     */
     private void loadNote() {
+
+        // 根据mNoteId在数据库中检索相应的row
         Cursor cursor = mContext.getContentResolver().query(
                 ContentUris.withAppendedId(Notes.CONTENT_NOTE_URI, mNoteId), NOTE_PROJECTION, null,
                 null, null);
@@ -146,7 +173,17 @@ public class WorkingNote {
         loadNoteData();
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: loadNoteData
+     * @description: 加载NoteData
+     * @param:
+     * @return: void
+     * @throws: IllegalArgumentException
+     */
     private void loadNoteData() {
+
+        // 在数据库中检索NOTE_ID=mNoteId的row
         Cursor cursor = mContext.getContentResolver().query(Notes.CONTENT_DATA_URI, DATA_PROJECTION,
                 DataColumns.NOTE_ID + "=?", new String[] {
                     String.valueOf(mNoteId)
@@ -156,11 +193,11 @@ public class WorkingNote {
             if (cursor.moveToFirst()) {
                 do {
                     String type = cursor.getString(DATA_MIME_TYPE_COLUMN);
-                    if (DataConstants.NOTE.equals(type)) {
+                    if (DataConstants.NOTE.equals(type)) { // 加载文本数据
                         mContent = cursor.getString(DATA_CONTENT_COLUMN);
                         mMode = cursor.getInt(DATA_MODE_COLUMN);
                         mNote.setTextDataId(cursor.getLong(DATA_ID_COLUMN));
-                    } else if (DataConstants.CALL_NOTE.equals(type)) {
+                    } else if (DataConstants.CALL_NOTE.equals(type)) { // 加载调用数据
                         mNote.setCallDataId(cursor.getLong(DATA_ID_COLUMN));
                     } else {
                         Log.d(TAG, "Wrong note type with type:" + type);
@@ -174,6 +211,14 @@ public class WorkingNote {
         }
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: createEmptyNote
+     * @description: 创建空的Note
+     * @param: Context context, long folderId, int widgetId, int widgetType, int defaultBgColorId
+     * @return: WorkingNote
+     * @throws:
+     */
     public static WorkingNote createEmptyNote(Context context, long folderId, int widgetId,
             int widgetType, int defaultBgColorId) {
         WorkingNote note = new WorkingNote(context, folderId);
@@ -183,13 +228,29 @@ public class WorkingNote {
         return note;
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: load
+     * @description: 根据id加载已有的Note
+     * @param: Context context, long id
+     * @return: WorkingNote
+     * @throws:
+     */
     public static WorkingNote load(Context context, long id) {
         return new WorkingNote(context, id, 0);
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: saveNote
+     * @description: 保存Note到数据库；若返回True，则代表保存成功；若返回False，代表保存失败
+     * @param:
+     * @return: boolean
+     * @throws:
+     */
     public synchronized boolean saveNote() {
-        if (isWorthSaving()) {
-            if (!existInDatabase()) {
+        if (isWorthSaving()) { // 判断是否值得保存
+            if (!existInDatabase()) { // 若在数据库中不存在
                 if ((mNoteId = Note.getNewNoteId(mContext, mFolderId)) == 0) {
                     Log.e(TAG, "Create new note fail with id:" + mNoteId);
                     return false;
@@ -212,11 +273,28 @@ public class WorkingNote {
         }
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: existInDatabase
+     * @description: 判断Note是否在数据库中存在；若存在，返回True；若不存在，返回False
+     * @param:
+     * @return: boolean
+     * @throws:
+     */
     public boolean existInDatabase() {
         return mNoteId > 0;
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: isWorthSaving
+     * @description: 判断Note是否值得保存；若值得，返回True；若不值得，返回False
+     * @param:
+     * @return: boolean
+     * @throws:
+     */
     private boolean isWorthSaving() {
+        //在以下三种情况下，WorkingNote不值得保存：1）被删除；2）在数据库中不存在且文本内容为空；3）在数据库中存在但未被本地修改
         if (mIsDeleted || (!existInDatabase() && TextUtils.isEmpty(mContent))
                 || (existInDatabase() && !mNote.isLocalModified())) {
             return false;
@@ -225,20 +303,44 @@ public class WorkingNote {
         }
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: setOnSettingStatusChangedListener
+     * @description: 设置NoteSettingChangedListener
+     * @param: NoteSettingChangedListener l
+     * @return: void
+     * @throws:
+     */
     public void setOnSettingStatusChangedListener(NoteSettingChangedListener l) {
         mNoteSettingStatusListener = l;
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: setAlertDate
+     * @description: 设置提醒日期
+     * @param: long date, boolean set
+     * @return: void
+     * @throws:
+     */
     public void setAlertDate(long date, boolean set) {
         if (date != mAlertDate) {
             mAlertDate = date;
-            mNote.setNoteValue(NoteColumns.ALERTED_DATE, String.valueOf(mAlertDate));
+            mNote.setNoteValue(NoteColumns.ALERTED_DATE, String.valueOf(mAlertDate)); // 更新提醒日期
         }
         if (mNoteSettingStatusListener != null) {
             mNoteSettingStatusListener.onClockAlertChanged(date, set);
         }
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: markDeleted
+     * @description: 删除Note
+     * @param: boolean mark
+     * @return: void
+     * @throws:
+     */
     public void markDeleted(boolean mark) {
         mIsDeleted = mark;
         if (mWidgetId != AppWidgetManager.INVALID_APPWIDGET_ID
@@ -247,6 +349,14 @@ public class WorkingNote {
         }
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: setBgColorId
+     * @description: 设置BgColorId
+     * @param: int id
+     * @return: void
+     * @throws:
+     */
     public void setBgColorId(int id) {
         if (id != mBgColorId) {
             mBgColorId = id;
@@ -257,6 +367,14 @@ public class WorkingNote {
         }
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: setCheckListMode
+     * @description: 设置文本数据的MODE；若MODE=1，表示在check list；若MODE=0，表示不在check list
+     * @param: int mode
+     * @return: void
+     * @throws:
+     */
     public void setCheckListMode(int mode) {
         if (mMode != mode) {
             if (mNoteSettingStatusListener != null) {
@@ -267,6 +385,14 @@ public class WorkingNote {
         }
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: setWidgetType
+     * @description: 设置WidgetType
+     * @param: int type
+     * @return: void
+     * @throws:
+     */
     public void setWidgetType(int type) {
         if (type != mWidgetType) {
             mWidgetType = type;
@@ -274,6 +400,14 @@ public class WorkingNote {
         }
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: setWidgetId
+     * @description: 设置WidgetId
+     * @param: int id
+     * @return: void
+     * @throws:
+     */
     public void setWidgetId(int id) {
         if (id != mWidgetId) {
             mWidgetId = id;
@@ -281,6 +415,14 @@ public class WorkingNote {
         }
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: setWorkingText
+     * @description: 设置文本
+     * @param: String text
+     * @return: void
+     * @throws:
+     */
     public void setWorkingText(String text) {
         if (!TextUtils.equals(mContent, text)) {
             mContent = text;
@@ -288,73 +430,209 @@ public class WorkingNote {
         }
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: convertToCallNote
+     * @description: 转化为通话数据
+     * @param: String phoneNumber, long callDate
+     * @return: void
+     * @throws:
+     */
     public void convertToCallNote(String phoneNumber, long callDate) {
-        mNote.setCallData(CallNote.CALL_DATE, String.valueOf(callDate));
-        mNote.setCallData(CallNote.PHONE_NUMBER, phoneNumber);
-        mNote.setNoteValue(NoteColumns.PARENT_ID, String.valueOf(Notes.ID_CALL_RECORD_FOLDER));
+        mNote.setCallData(CallNote.CALL_DATE, String.valueOf(callDate)); // 设置通话日期
+        mNote.setCallData(CallNote.PHONE_NUMBER, phoneNumber); // 设置通话号码
+        mNote.setNoteValue(NoteColumns.PARENT_ID, String.valueOf(Notes.ID_CALL_RECORD_FOLDER)); // 将Note存到专门存放通话数据的文件夹中
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: hasClockAlert
+     * @description: 判断是否有闹钟提醒；若有，返回True；若没有，则返回False
+     * @param:
+     * @return: boolean
+     * @throws:
+     */
     public boolean hasClockAlert() {
         return (mAlertDate > 0 ? true : false);
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: getContent
+     * @description: 获取文本
+     * @param:
+     * @return: String
+     * @throws:
+     */
     public String getContent() {
         return mContent;
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: getAlertDate
+     * @description: 获取提醒日期
+     * @param:
+     * @return: long
+     * @throws:
+     */
     public long getAlertDate() {
         return mAlertDate;
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: getModifiedDate
+     * @description: 获取修改日期
+     * @param:
+     * @return: long
+     * @throws:
+     */
     public long getModifiedDate() {
         return mModifiedDate;
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: getBgColorResId
+     * @description: 获取便签背景颜色资源ID
+     * @param:
+     * @return: int
+     * @throws:
+     */
     public int getBgColorResId() {
         return NoteBgResources.getNoteBgResource(mBgColorId);
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: getBgColorId
+     * @description: 获取便签背景颜色ID
+     * @param:
+     * @return: int
+     * @throws:
+     */
     public int getBgColorId() {
         return mBgColorId;
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: getTitleBgResId
+     * @description: 获取标题背景颜色ID
+     * @param:
+     * @return: int
+     * @throws:
+     */
     public int getTitleBgResId() {
         return NoteBgResources.getNoteTitleBgResource(mBgColorId);
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: getCheckListMode
+     * @description: 获取CheckListMode；若Note在check list中，返回True；若Note不在check list中，返回False
+     * @param:
+     * @return: int
+     * @throws:
+     */
     public int getCheckListMode() {
         return mMode;
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: getNoteId
+     * @description: 获取Note对应的ID
+     * @param:
+     * @return: long
+     * @throws:
+     */
     public long getNoteId() {
         return mNoteId;
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: getFolderId
+     * @description: 获取Note所在文件夹的ID
+     * @param:
+     * @return: long
+     * @throws:
+     */
     public long getFolderId() {
         return mFolderId;
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: getWidgetId
+     * @description: 获取WidgetId
+     * @param:
+     * @return: int
+     * @throws:
+     */
     public int getWidgetId() {
         return mWidgetId;
     }
 
+    /**
+     * @author:  Yi Huang
+     * @methodsName: getWidgetType
+     * @description: 获取WidgetType
+     * @param:
+     * @return: int
+     * @throws:
+     */
     public int getWidgetType() {
         return mWidgetType;
     }
 
+    /**
+     * @version: V1.0
+     * @author: Yi Huang
+     * @className: NoteSettingChangedListener
+     * @packageName: model
+     * @description: 监视便签的属性设置是否改变
+     * @data: 2023-03-07
+     **/
     public interface NoteSettingChangedListener {
         /**
          * Called when the background color of current note has just changed
+         */
+        /**
+         * @author:  Yi Huang
+         * @methodsName: onBackgroundColorChanged
+         * @description: 在当前笔记的背景颜色刚刚改变时被调用
+         * @param:
+         * @return: void
+         * @throws:
          */
         void onBackgroundColorChanged();
 
         /**
          * Called when user set clock
          */
+        /**
+         * @author:  Yi Huang
+         * @methodsName: onClockAlertChanged
+         * @description: 在用户设置闹钟时被调用
+         * @param:
+         * @return: void
+         * @throws:
+         */
         void onClockAlertChanged(long date, boolean set);
 
         /**
          * Call when user create note from widget
+         */
+        /**
+         * @author:  Yi Huang
+         * @methodsName: onWidgetChanged
+         * @description: 当用户从小部件创建笔记时被调用
+         * @param:
+         * @return: void
+         * @throws:
          */
         void onWidgetChanged();
 
@@ -362,6 +640,14 @@ public class WorkingNote {
          * Call when switch between check list mode and normal mode
          * @param oldMode is previous mode before change
          * @param newMode is new mode
+         */
+        /**
+         * @author:  Yi Huang
+         * @methodsName: onCheckListModeChanged
+         * @description: 清单模式和普通模式切换时被调用
+         * @param: int oldMode, int newMode
+         * @return: void
+         * @throws:
          */
         void onCheckListModeChanged(int oldMode, int newMode);
     }
