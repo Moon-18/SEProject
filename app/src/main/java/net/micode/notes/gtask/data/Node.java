c67351998e52152d000d8a2f09d70c63ff74cc31
Liuwq-bit <liuwq_bit@outlook.com>
2023-03-09 08:49:12 +0800

    3.2 lwq

--- app/src/main/java/net/micode/notes/gtask/data/Node.java
+++ app/src/main/java/net/micode/notes/gtask/data/Node.java
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/**
+ * @analyst: lwq
+ */
 
 package net.micode.notes.gtask.data;
 
@@ -20,33 +23,43 @@ import android.database.Cursor;
 
 import org.json.JSONObject;
 
+/**
+ * 同步操作所使用到的数据类型及方法
+ */
 public abstract class Node {
-    public static final int SYNC_ACTION_NONE = 0;
+    /**
+     * 以下静态常量代表不同的同步状态
+     * local：本地，remote：云端
+     */
+    public static final int SYNC_ACTION_NONE = 0;   // 本地及云端无可执行操作
 
-    public static final int SYNC_ACTION_ADD_REMOTE = 1;
+    public static final int SYNC_ACTION_ADD_REMOTE = 1; // 云端需添加内容
 
-    public static final int SYNC_ACTION_ADD_LOCAL = 2;
+    public static final int SYNC_ACTION_ADD_LOCAL = 2;  // 本地需添加内容
 
-    public static final int SYNC_ACTION_DEL_REMOTE = 3;
+    public static final int SYNC_ACTION_DEL_REMOTE = 3; // 云端需删除内容
 
-    public static final int SYNC_ACTION_DEL_LOCAL = 4;
+    public static final int SYNC_ACTION_DEL_LOCAL = 4;  // 本地需删除内容
 
-    public static final int SYNC_ACTION_UPDATE_REMOTE = 5;
+    public static final int SYNC_ACTION_UPDATE_REMOTE = 5;  // 云端需更新内容
 
-    public static final int SYNC_ACTION_UPDATE_LOCAL = 6;
+    public static final int SYNC_ACTION_UPDATE_LOCAL = 6;   // 本地需更新内容
 
-    public static final int SYNC_ACTION_UPDATE_CONFLICT = 7;
+    public static final int SYNC_ACTION_UPDATE_CONFLICT = 7;    // 同步更新过程中发生冲突
 
-    public static final int SYNC_ACTION_ERROR = 8;
+    public static final int SYNC_ACTION_ERROR = 8;  // 同步过程发生错误
 
-    private String mGid;
+    private String mGid;    // 消息id
 
-    private String mName;
+    private String mName;   // 消息名称
 
-    private long mLastModified;
+    private long mLastModified; // 最后一次修改时间
 
-    private boolean mDeleted;
+    private boolean mDeleted;   // 代表当前消息是否被删除
 
+    /**
+     * 消息节点对象
+     */
     public Node() {
         mGid = null;
         mName = "";
@@ -54,18 +67,24 @@ public abstract class Node {
         mDeleted = false;
     }
 
-    public abstract JSONObject getCreateAction(int actionId);
+    /**
+     * 以下方法为抽象类，此处仅做声明，需在子类中具体实现
+     */
+    public abstract JSONObject getCreateAction(int actionId);   // 给定actionId，创建并返回当前Node中的JSONObject对象
 
-    public abstract JSONObject getUpdateAction(int actionId);
+    public abstract JSONObject getUpdateAction(int actionId);   // 更新JSONObject
 
-    public abstract void setContentByRemoteJSON(JSONObject js);
+    public abstract void setContentByRemoteJSON(JSONObject js); // 给定远程Node的JSONObject，更新当前Node的内容
 
-    public abstract void setContentByLocalJSON(JSONObject js);
+    public abstract void setContentByLocalJSON(JSONObject js);  // 给定本地Node的JSONObject，更新当前Node的内容
 
     public abstract JSONObject getLocalJSONFromContent();
 
     public abstract int getSyncAction(Cursor c);
 
+    /**
+     * 以下方法为常规属性设置方法，此处不再标记
+     */
     public void setGid(String gid) {
         this.mGid = gid;
     }
--- app/src/main/java/net/micode/notes/gtask/data/Task.java
+++ app/src/main/java/net/micode/notes/gtask/data/Task.java
@@ -13,7 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+/**
+ * analyst: lwq
+ */
 package net.micode.notes.gtask.data;
 
 import android.database.Cursor;
@@ -31,20 +33,26 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
+/**
+ * 该类为抽象类Node的子类，包含其中方法的具体实现
+ * 并对其功能进行扩展
+ */
 public class Task extends Node {
-    private static final String TAG = Task.class.getSimpleName();
+    private static final String TAG = Task.class.getSimpleName();   // 获取当前类的名称，即Task，用于后续日志输出
 
-    private boolean mCompleted;
+    private boolean mCompleted; // 是否完成
 
-    private String mNotes;
+    private String mNotes;  // 便签内容
 
-    private JSONObject mMetaInfo;
+    private JSONObject mMetaInfo;   // 存储便签数据使用到的JSONObject实例
 
-    private Task mPriorSibling;
+    private Task mPriorSibling; // 前一个Task对象的指针
 
-    private TaskList mParent;
+    private TaskList mParent;   // 任务列表，存放所有的Task
 
+    /**
+     * Task实例对象
+     */
     public Task() {
         super();
         mCompleted = false;
@@ -54,32 +62,35 @@ public class Task extends Node {
         mMetaInfo = null;
     }
 
+    /**
+     * 给定actionId，创建并返回当前Node中的JSONObject对象
+     */
     public JSONObject getCreateAction(int actionId) {
         JSONObject js = new JSONObject();
 
         try {
-            // action_type
+            // 设置action_type为create
             js.put(GTaskStringUtils.GTASK_JSON_ACTION_TYPE,
                     GTaskStringUtils.GTASK_JSON_ACTION_TYPE_CREATE);
 
-            // action_id
+            // 设置action_id
             js.put(GTaskStringUtils.GTASK_JSON_ACTION_ID, actionId);
 
-            // index
+            // 根据所在TaskList，设置index
             js.put(GTaskStringUtils.GTASK_JSON_INDEX, mParent.getChildTaskIndex(this));
 
-            // entity_delta
+            // 创建entity_delta，存储笔记信息，并将其放入js中
             JSONObject entity = new JSONObject();
             entity.put(GTaskStringUtils.GTASK_JSON_NAME, getName());
             entity.put(GTaskStringUtils.GTASK_JSON_CREATOR_ID, "null");
             entity.put(GTaskStringUtils.GTASK_JSON_ENTITY_TYPE,
                     GTaskStringUtils.GTASK_JSON_TYPE_TASK);
-            if (getNotes() != null) {
+            if (getNotes() != null) { // 所更新消息不为null时放入消息
                 entity.put(GTaskStringUtils.GTASK_JSON_NOTES, getNotes());
             }
             js.put(GTaskStringUtils.GTASK_JSON_ENTITY_DELTA, entity);
 
-            // parent_id
+            // 标识当前Task所在的TaskList，parent_id
             js.put(GTaskStringUtils.GTASK_JSON_PARENT_ID, mParent.getGid());
 
             // dest_parent_type
@@ -89,12 +100,12 @@ public class Task extends Node {
             // list_id
             js.put(GTaskStringUtils.GTASK_JSON_LIST_ID, mParent.getGid());
 
-            // prior_sibling_id
+            // prior_sibling_id，不为该List中首个对象时更新指针
             if (mPriorSibling != null) {
                 js.put(GTaskStringUtils.GTASK_JSON_PRIOR_SIBLING_ID, mPriorSibling.getGid());
             }
 
-        } catch (JSONException e) {
+        } catch (JSONException e) { // 出现异常则在log中显示错误信息
             Log.e(TAG, e.toString());
             e.printStackTrace();
             throw new ActionFailureException("fail to generate task-create jsonobject");
@@ -103,30 +114,33 @@ public class Task extends Node {
         return js;
     }
 
+    /**
+     * 更新JSONObject
+     */
     public JSONObject getUpdateAction(int actionId) {
         JSONObject js = new JSONObject();
 
         try {
-            // action_type
+            // 设置action_type为更新状态
             js.put(GTaskStringUtils.GTASK_JSON_ACTION_TYPE,
                     GTaskStringUtils.GTASK_JSON_ACTION_TYPE_UPDATE);
 
-            // action_id
+            // 更新action_id
             js.put(GTaskStringUtils.GTASK_JSON_ACTION_ID, actionId);
 
-            // id
+            // 更新id
             js.put(GTaskStringUtils.GTASK_JSON_ID, getGid());
 
-            // entity_delta
+            // 更新entity_delta
             JSONObject entity = new JSONObject();
             entity.put(GTaskStringUtils.GTASK_JSON_NAME, getName());
-            if (getNotes() != null) {
+            if (getNotes() != null) {   // 新便签内容不为null时更新
                 entity.put(GTaskStringUtils.GTASK_JSON_NOTES, getNotes());
             }
             entity.put(GTaskStringUtils.GTASK_JSON_DELETED, getDeleted());
             js.put(GTaskStringUtils.GTASK_JSON_ENTITY_DELTA, entity);
 
-        } catch (JSONException e) {
+        } catch (JSONException e) { // 出现异常则在log中输出
             Log.e(TAG, e.toString());
             e.printStackTrace();
             throw new ActionFailureException("fail to generate task-update jsonobject");
@@ -135,6 +149,9 @@ public class Task extends Node {
         return js;
     }
 
+    /**
+     * 给定远程Node的JSONObject，更新当前Node的内容
+     */
     public void setContentByRemoteJSON(JSONObject js) {
         if (js != null) {
             try {
@@ -175,9 +192,12 @@ public class Task extends Node {
         }
     }
 
+    /**
+     * 给定本地Node的JSONObject，更新当前Node的内容
+     */
     public void setContentByLocalJSON(JSONObject js) {
         if (js == null || !js.has(GTaskStringUtils.META_HEAD_NOTE)
-                || !js.has(GTaskStringUtils.META_HEAD_DATA)) {
+                || !js.has(GTaskStringUtils.META_HEAD_DATA)) {  // 判断是否可用
             Log.w(TAG, "setContentByLocalJSON: nothing is avaiable");
         }
 
@@ -185,12 +205,12 @@ public class Task extends Node {
             JSONObject note = js.getJSONObject(GTaskStringUtils.META_HEAD_NOTE);
             JSONArray dataArray = js.getJSONArray(GTaskStringUtils.META_HEAD_DATA);
 
-            if (note.getInt(NoteColumns.TYPE) != Notes.TYPE_NOTE) {
+            if (note.getInt(NoteColumns.TYPE) != Notes.TYPE_NOTE) { // 类型需一致
                 Log.e(TAG, "invalid type");
                 return;
             }
 
-            for (int i = 0; i < dataArray.length(); i++) {
+            for (int i = 0; i < dataArray.length(); i++) {  // 将js中的内容依次复制过来
                 JSONObject data = dataArray.getJSONObject(i);
                 if (TextUtils.equals(data.getString(DataColumns.MIME_TYPE), DataConstants.NOTE)) {
                     setName(data.getString(DataColumns.CONTENT));
@@ -204,10 +224,13 @@ public class Task extends Node {
         }
     }
 
+    /**
+     * 根据当前笔记来获取JSONObject对象
+     */
     public JSONObject getLocalJSONFromContent() {
         String name = getName();
         try {
-            if (mMetaInfo == null) {
+            if (mMetaInfo == null) {    // 云端创建
                 // new task created from web
                 if (name == null) {
                     Log.w(TAG, "the note seems to be an empty one");
@@ -224,12 +247,12 @@ public class Task extends Node {
                 note.put(NoteColumns.TYPE, Notes.TYPE_NOTE);
                 js.put(GTaskStringUtils.META_HEAD_NOTE, note);
                 return js;
-            } else {
+            } else {    // 已存在本地note，与云端同步
                 // synced task
                 JSONObject note = mMetaInfo.getJSONObject(GTaskStringUtils.META_HEAD_NOTE);
                 JSONArray dataArray = mMetaInfo.getJSONArray(GTaskStringUtils.META_HEAD_DATA);
 
-                for (int i = 0; i < dataArray.length(); i++) {
+                for (int i = 0; i < dataArray.length(); i++) {  // todo 暂时没看懂
                     JSONObject data = dataArray.getJSONObject(i);
                     if (TextUtils.equals(data.getString(DataColumns.MIME_TYPE), DataConstants.NOTE)) {
                         data.put(DataColumns.CONTENT, getName());
@@ -247,8 +270,11 @@ public class Task extends Node {
         }
     }
 
+    /**
+     * 传入新的JSONObject数据
+     */
     public void setMetaInfo(MetaData metaData) {
-        if (metaData != null && metaData.getNotes() != null) {
+        if (metaData != null && metaData.getNotes() != null) {  // 传入数据不为null时更新当前数据
             try {
                 mMetaInfo = new JSONObject(metaData.getNotes());
             } catch (JSONException e) {
@@ -258,19 +284,22 @@ public class Task extends Node {
         }
     }
 
-    public int getSyncAction(Cursor c) {
+    /**
+     * 获取同步操作
+     */
+    public int getSyncAction(Cursor c) {    // Cursor类型用于指向数据所在位置
         try {
             JSONObject noteInfo = null;
-            if (mMetaInfo != null && mMetaInfo.has(GTaskStringUtils.META_HEAD_NOTE)) {
+            if (mMetaInfo != null && mMetaInfo.has(GTaskStringUtils.META_HEAD_NOTE)) {  // 本地note存在时，noteInfo设为当前note
                 noteInfo = mMetaInfo.getJSONObject(GTaskStringUtils.META_HEAD_NOTE);
             }
 
-            if (noteInfo == null) {
+            if (noteInfo == null) { // 无法获取本地note，
                 Log.w(TAG, "it seems that note meta has been deleted");
                 return SYNC_ACTION_UPDATE_REMOTE;
             }
 
-            if (!noteInfo.has(NoteColumns.ID)) {
+            if (!noteInfo.has(NoteColumns.ID)) {    // todo 暂时没看懂
                 Log.w(TAG, "remote note id seems to be deleted");
                 return SYNC_ACTION_UPDATE_LOCAL;
             }
@@ -311,6 +340,9 @@ public class Task extends Node {
         return SYNC_ACTION_ERROR;
     }
 
+    /**
+     * 判断是否需要保存
+     */
     public boolean isWorthSaving() {
         return mMetaInfo != null || (getName() != null && getName().trim().length() > 0)
                 || (getNotes() != null && getNotes().trim().length() > 0);
